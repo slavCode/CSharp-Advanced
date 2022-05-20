bcb17d9fd897cda48e0f879df119401cd7ade48b
slavCode <stomow@gmail.com>
2022-05-20 10:35:54 +0300

    Add New Lab Exercises

--- 01 STACKS AND QUEUES/Stacks-And-Queues-Lab/DecimalToBinaryConverter/Program.cs
+++ 01 STACKS AND QUEUES/Stacks-And-Queues-Lab/DecimalToBinaryConverter/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace DecimalToBinaryConverter
 {
-    internal class Program
+    internal class SimpleCalculator
     {
         static void Main()
         {
