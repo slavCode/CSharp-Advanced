c63d0e04d65b7f7e1faa05e4b88253740f18686a
slavCode <stomow@gmail.com>
2022-05-20 11:01:57 +0300

    Reverse Numbers With A Stack

---
+++ 01 STACKS AND QUEUES/Stacks-And-Queues-Exercises/E01-ReverseNumbersWithAStack/ReverseNumbersWithAStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace E01_ReverseNumbersWithAStack
+{
+    class ReverseNumbersWithAStack
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine()
+                          .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Select(int.Parse)
+                          .ToArray();
+
+            var reversedNumbers = new Stack<int>();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                reversedNumbers.Push(numbers[i]);
+            }
+
+            while (reversedNumbers.Count > 0)
+            {
+                Console.Write($"{reversedNumbers.Pop()} ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
