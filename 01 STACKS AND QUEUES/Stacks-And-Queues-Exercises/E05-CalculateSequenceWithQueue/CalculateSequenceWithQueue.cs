fd3eaa0a90dbdaabddde77cc67c2982ae231b869
slavCode <stomow@gmail.com>
2022-05-26 16:09:34 +0300

    Add Truck Tour

---
+++ 01 STACKS AND QUEUES/Stacks-And-Queues-Exercises/E05-CalculateSequenceWithQueue/CalculateSequenceWithQueue.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E05_CalculateSequenceWithQueue
+{
+    class CalculateSequenceWithQueue
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            var calculatedSequences = new List<Queue<int>>();
+
+            var currentSequence = new Queue<int>();
+
+            for (int i = 0; i <= 50; i++)
+            {
+
+                int nextNumber = number + 2;
+
+                currentSequence.Enqueue(number);
+                currentSequence.Enqueue(number + 1);
+                currentSequence.Enqueue(2 * number + 1);
+                currentSequence.Enqueue(nextNumber);
+
+                number = nextNumber;
+            }
+        }
+    }
+}
---
+++ 01 STACKS AND QUEUES/Stacks-And-Queues-Exercises/E06-TruckTour/TruckTour.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace E06_TruckTour
+{
+    internal class TruckTour
+    {
+        static void Main()
+        {
+            int numOfPumps = int.Parse(Console.ReadLine());
+            var pumps = new Queue<Pump>();
+
+            for (int i = 0; i < numOfPumps; i++)
+            {
+                var pumpParams = Console.ReadLine().Split();
+                int distanceToNext = int.Parse(pumpParams[1]);
+                int gas = int.Parse(pumpParams[0]);
+
+                Pump pump = new Pump(i, distanceToNext, gas);
+                pumps.Enqueue(pump);
+            }
+
+            Pump startPump = null;
+            bool completeJorney = false;
+            while (!completeJorney)
+            {
+                startPump = pumps.Dequeue();
+                pumps.Enqueue(startPump);
+
+                int gasInTank = startPump.Gas;
+                while (gasInTank >= startPump.DistanceToNext)
+                {
+                    Pump currPump = pumps.Dequeue();
+                    pumps.Enqueue(currPump);
+
+                    gasInTank += currPump.Gas;
+                    gasInTank -= currPump.DistanceToNext;
+
+                    if (currPump == startPump)
+                    {
+                        completeJorney = true;
+                        break;
+                    }
+                }
+
+                if (completeJorney) break;
+            }
+            Console.WriteLine(startPump.Index);
+        }
+
+        class Pump
+        {
+            public int Index { get; set; }
+            public int DistanceToNext { get; set; }
+            public int Gas { get; set; }
+            public Pump(int index, int distanceToNext, int gas)
+            {
+                this.Index = index;
+                this.DistanceToNext = distanceToNext;
+                this.Gas = gas;
+            }
+        }
+    }
+}
