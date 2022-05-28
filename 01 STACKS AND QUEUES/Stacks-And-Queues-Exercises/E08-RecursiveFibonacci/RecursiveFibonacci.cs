f85a559948f00789d97503c7f19f3f951f4a6f69
Slav Tomov <stomow@outlook.com>
2022-05-28 14:14:17 +0300

    Recursive Fibonacci

---
+++ 01 STACKS AND QUEUES/Stacks-And-Queues-Exercises/E08-RecursiveFibonacci/RecursiveFibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace E08_RecursiveFibonacci
+{
+    class RecursiveFibonacci
+    {
+        private static long[] fibonacchiNumbers;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            fibonacchiNumbers = new long[n];
+            long result = GetFibonacci(n);
+            Console.WriteLine(result);
+        }
+
+        static long GetFibonacci(int n)
+        {
+            if (n < 2) return n;
+            if (fibonacchiNumbers[n - 1] != 0) return fibonacchiNumbers[n - 1];
+            return fibonacchiNumbers[n - 1] = GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
