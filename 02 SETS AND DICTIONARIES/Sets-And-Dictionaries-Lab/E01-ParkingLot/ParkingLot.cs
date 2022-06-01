d1e5ea761be622d4136d84d7d214dc8ebea90e23
Slav Tomov <stomow@outlook.com>
2022-06-01 10:44:06 +0300

    ParkingLot

---
+++ 02 SETS AND DICTIONARIES/Sets-And-Dictionaries-Lab/E01-ParkingLot/ParkingLot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class ParkingLot
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine();
+        HashSet<string> cars = new HashSet<string>();
+
+        while (input != "END")
+        {
+            var args = input.Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries);
+            string direction = args[0];
+            string carPlate = args[1];
+
+            switch (direction)
+            {
+                case "IN":
+                    cars.Add(carPlate);
+                    break;
+                case "OUT":
+                    cars.Remove(carPlate);
+                    break;
+            }
+
+            input = Console.ReadLine();
+        }
+
+        if (cars.Count > 0)
+        {
+            Console.WriteLine($"{string.Join("\n", cars)}");
+        }
+        else
+        {
+            Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
+
