e384cc43fca81df74cf73fe77be3fea552941274
Slav Tomov <stomow@outlook.com>
2022-06-01 18:47:38 +0300

    Unique Usernames

---
+++ 02 SETS AND DICTIONARIES/Sets-And-Dictionaries-Exercises/E01-UniqueUsernames/UniqueUsernames.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class UniqueUsernames
+{
+    public static void Main()
+    {
+        int numberOfUsers = int.Parse(Console.ReadLine());
+        HashSet<string> users = new HashSet<string>();
+
+        for (int i = 0; i < numberOfUsers; i++)
+        {
+            string user = Console.ReadLine();
+            users.Add(user);
+        }
+        Console.WriteLine($"{string.Join("\n", users)}");
+    }
+
+}
