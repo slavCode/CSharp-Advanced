c4b0e4b2dfb348b6b9115ff98cad67d202f58809
Slav Tomov <stomow@outlook.com>
2022-06-01 13:05:20 +0300

    Count Same Values In Array

---
+++ 02 SETS AND DICTIONARIES/Sets-And-Dictionaries-Lab/L03-CountSameValuesInArray/CountSameValuesInArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class CountSameValuesInArray
+{
+    public static void Main()
+    {
+        var input = Console.ReadLine()
+                   .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                   .Select(double.Parse);
+        var values = new Dictionary<double, int>();
+
+        foreach (var value in input)
+        {
+            if (!values.ContainsKey(value)) values.Add(value, 1);
+            else values[value]++;
+        }
+
+        foreach (var value in values)
+        {
+            Console.WriteLine($"{value.Key} - {value.Value} times");
+        }
+    }
+}
