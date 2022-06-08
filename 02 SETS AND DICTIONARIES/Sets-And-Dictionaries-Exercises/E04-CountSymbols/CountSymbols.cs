3b789a7a30358d19ba352c199674ae56a54c9452
slavCode <stomow@gmail.com>
2022-06-08 13:44:55 +0300

    Count Symbols

---
+++ 02 SETS AND DICTIONARIES/Sets-And-Dictionaries-Exercises/E04-CountSymbols/CountSymbols.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CountSymbols
+{
+    public static void Main()
+    {
+        var text = Console.ReadLine();
+        var letters = new SortedDictionary<char, int>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            char letter = text[i];
+            if (!letters.ContainsKey(letter))
+            {
+                letters.Add(letter, 1);
+            }
+            else letters[letter]++;
+        }
+
+        foreach (var letter in letters)
+        {
+            Console.WriteLine($"{letter.Key}: {letter.Value} time/s");
+        }
+    }
+}
